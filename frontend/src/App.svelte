2ba89a25b6df268e535cabe22cf3ae5215b85dcb
Franco Magrini <franco.mgr@gmail.com>
2023-12-08 14:21:12 -0300

    frontend

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,10 +1,13 @@
 <script>
     import tabler from 'yesvelte/css/tabler.min.css?url';
+    import {El} from "yesvelte";
+
     import Login from './lib/components/Login.svelte';
     import Pag1 from './lib/components/Pag1.svelte';
     import Pag2 from './lib/components/Pag2.svelte';
     import Settings from './lib/components/Settings.svelte';
-    import {SidebarItem, Sidebar, El, ButtonGroup,Button, Icon} from "yesvelte";
+    import SideNavBar from './lib/components/SideNavBar.svelte';
+    import TopNavBar from './lib/components/TopNavBar.svelte';
 
     let isLogin = false
     let selectedMenu = "login";
@@ -16,46 +19,31 @@
 </svelte:head>
 
 <main data-theme="dark" data-bs-theme="dark">
-    <El container style="overflow: hidden">
-        <El row>
-            <El col="2">
-                <Sidebar theme="dark" style="width: fit-content">
-                    {#if isLogin}
-                    <ButtonGroup col="auto">
-                        <Button>
-                            <Icon name="settings" on:click={() => (selectedMenu = "settings")}/>
-                        </Button>
-                        <Button>
-                            <Icon name="user" />
-                        </Button>
-                    </ButtonGroup>
-                    {/if}
-                    {#if !isLogin}
-                        <SidebarItem icon="login" title="Identificarse" on:click={() => (selectedMenu = "login")}/>
-                    {:else}
-                        <SidebarItem icon="file" title="Page 1" on:click={() => (selectedMenu = "pag1")}/>
-                        <SidebarItem icon="file" title="Page 2" on:click={() => (selectedMenu = "pag2")}/>
-                    {/if}
-                </Sidebar>
-            </El>
-            <El col mt="5">
-                {#if isLogin}
-                    {#if selectedMenu === "pag1"}
-                        <Pag1 />
-                    {:else if selectedMenu === "pag2"}
-                        <Pag2 />
-                    {:else if selectedMenu === "settings"}
-                        <Settings />
-                    {:else}
-                        <h1>
-                            Page Not Found
-                        </h1>
-                    {/if}
-                {:else}
-                    <Login bind:loggedIn={isLogin} bind:redirectAfterLogin={selectedMenu}/>
-                {/if}
-            </El>
-        </El>
-    </El>
+    <TopNavBar />
+    <SideNavBar {isLogin} {selectedMenu} on:menuChange={(e) => (selectedMenu = e.detail)}/>
+
+    {#if isLogin}
+        {#if selectedMenu === "pag1"}
+            <Pag1 />
+        {:else if selectedMenu === "pag2"}
+            <Pag2 />
+        {:else if selectedMenu === "settings"}
+            <Settings />
+        {:else}
+            <h1>
+                Page Not Found
+            </h1>
+        {/if}
+    {:else}
+        <Login bind:loggedIn={isLogin} bind:redirectAfterLogin={selectedMenu}/>
+    {/if}
 </main>
+
+<style>
+    main {
+        height: 100vh;
+        width: 100vw;
+        overflow: hidden;
+    }
+</style>
 
---
+++ frontend/src/lib/components/TopNavBar.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { Navbar, NavbarItem } from "yesvelte";
+</script>
+
+<Navbar theme="dark">
+    <NavbarItem title="Home">
+    </NavbarItem >
+    <NavbarItem title="Features" >
+    </NavbarItem >
+    <NavbarItem title="Pricing">
+    </NavbarItem >
+</Navbar>
